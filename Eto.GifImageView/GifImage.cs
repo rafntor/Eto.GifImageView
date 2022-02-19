b00c1fd3e43ec65a8db572b3eb122caaa95e6f64
Tore Fuglestad <tore@fuglestad.no>
2022-02-19 19:16:16 +0100

    do the main reimplementation/porting

--- Eto.GifImageView/GifImage.cs
+++ Eto.GifImageView/GifImage.cs
@@ -1,184 +1,64 @@
-﻿using System;
-using System.IO;
-using Avalonia;
-using Avalonia.Animation;
-using Avalonia.Controls;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
-using Avalonia.Threading;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
 
-namespace AvaloniaGif
+namespace Eto.GifImageView
 {
-    public class GifImage : Control
-    {
-        public static readonly StyledProperty<string> SourceUriRawProperty = AvaloniaProperty.Register<GifImage, string>("SourceUriRaw");
-        public static readonly StyledProperty<Uri> SourceUriProperty = AvaloniaProperty.Register<GifImage, Uri>("SourceUri");
-        public static readonly StyledProperty<Stream> SourceStreamProperty = AvaloniaProperty.Register<GifImage, Stream>("SourceStream");
-        public static readonly StyledProperty<IterationCount> IterationCountProperty = AvaloniaProperty.Register<GifImage, IterationCount>("IterationCount");
-        private GifInstance gifInstance;
-        public static readonly StyledProperty<bool> AutoStartProperty = AvaloniaProperty.Register<GifImage, bool>("AutoStart");
-        public static readonly StyledProperty<StretchDirection> StretchDirectionProperty = AvaloniaProperty.Register<GifImage, StretchDirection>("StretchDirection");
-        public static readonly StyledProperty<Stretch> StretchProperty = AvaloniaProperty.Register<GifImage, Stretch>("Stretch");
-        private RenderTargetBitmap backingRTB;
+	using System;
+	using System.IO;
+	using System.Reflection;
+	using Eto.Drawing;
+	using Eto.GifImageView.Decoding;
+	public class GifImage : IDisposable
+	{
+		private GifDecoder _gifDecoder;
+		private GifBackgroundWorker _bgWorker;
+		private Bitmap _targetBitmap;
+		private bool _isDisposed;
 
-        static GifImage()
-        {
-            SourceUriRawProperty.Changed.Subscribe(SourceChanged);
-            SourceUriProperty.Changed.Subscribe(SourceChanged);
-            SourceStreamProperty.Changed.Subscribe(SourceChanged);
-            IterationCountProperty.Changed.Subscribe(IterationCountChanged);
-            AutoStartProperty.Changed.Subscribe(AutoStartChanged);
-            AffectsRender<GifImage>(SourceStreamProperty, SourceUriProperty, SourceUriRawProperty);
-            AffectsArrange<GifImage>(SourceStreamProperty, SourceUriProperty, SourceUriRawProperty);
-            AffectsMeasure<GifImage>(SourceStreamProperty, SourceUriProperty, SourceUriRawProperty);
-        }
+		public Bitmap CurrentBitmap { get => _targetBitmap; }
 
-        public string SourceUriRaw
-        {
-            get => GetValue(SourceUriRawProperty);
-            set => SetValue(SourceUriRawProperty, value);
-        }
+		public event EventHandler<EventArgs> OnFrameChanged;
 
-        public Uri SourceUri
-        {
-            get => GetValue(SourceUriProperty);
-            set => SetValue(SourceUriProperty, value);
-        }
+		public static GifImage FromFile(string path)
+		{
+			using var stream = File.OpenRead(path);
 
-        public Stream SourceStream
-        {
-            get => GetValue(SourceStreamProperty);
-            set => SetValue(SourceStreamProperty, value);
-        }
+			return FromStream(stream);
+		}
+		public static GifImage FromResource(string resourceName, Assembly assembly = null)
+		{
+			if (assembly is null)
+				assembly = Assembly.GetCallingAssembly();
 
-        public IterationCount IterationCount
-        {
-            get => GetValue(IterationCountProperty);
-            set => SetValue(IterationCountProperty, value);
-        }
+			return FromStream(assembly.GetManifestResourceStream(resourceName));
+		}
+		public static GifImage FromStream(Stream stream) => new GifImage(stream);
+		private GifImage(Stream stream)
+		{
+			if (!stream.CanSeek)
+				throw new ArgumentException("The stream is not seekable");
 
-        public bool AutoStart
-        {
-            get => GetValue(AutoStartProperty);
-            set => SetValue(AutoStartProperty, value);
-        }
+			_gifDecoder = new GifDecoder(stream);
+			_bgWorker = new GifBackgroundWorker(_gifDecoder);
+			_targetBitmap = new Bitmap(_gifDecoder.Header.Dimensions.Width, _gifDecoder.Header.Dimensions.Height, PixelFormat.Format32bppRgba);
+			_bgWorker.CurrentFrameChanged += FrameChanged;
+			_bgWorker.SendCommand(BgWorkerCommand.Play);
+		}
 
-        public StretchDirection StretchDirection
-        {
-            get => GetValue(StretchDirectionProperty);
-            set => SetValue(StretchDirectionProperty, value);
-        }
+		private void FrameChanged()
+		{
+			if (_isDisposed) return;
 
-        public Stretch Stretch
-        {
-            get => GetValue(StretchProperty);
-            set => SetValue(StretchProperty, value);
-        }
+			using (var lockedBitmap = _targetBitmap?.Lock())
+				_gifDecoder?.WriteBackBufToFb(lockedBitmap.Data);
 
-        private static void AutoStartChanged(AvaloniaPropertyChangedEventArgs e)
-        {
-            var image = e.Sender as GifImage;
-            if (image == null)
-                return;
-        }
+			if (OnFrameChanged != null)
+				Eto.Forms.Application.Instance.InvokeAsync(() => OnFrameChanged.Invoke(this,EventArgs.Empty));
+		}
 
-        private static void IterationCountChanged(AvaloniaPropertyChangedEventArgs e)
-        {
-            var image = e.Sender as GifImage;
-            if (image == null)
-                return; 
-        }
-
-        public override void Render(DrawingContext context)
-        {
-            if (gifInstance == null)
-                return;
-                
-            if (gifInstance.GetBitmap() is WriteableBitmap source && backingRTB is not null)
-            {
-                using (var ctx = backingRTB.CreateDrawingContext(null))
-                {
-                    var ts = new Rect(source.Size);
-                    ctx.DrawBitmap(source.PlatformImpl, 1, ts,ts);
-                }
-            }
-            if (backingRTB is not null && Bounds.Width > 0 && Bounds.Height > 0)
-            {
-                var viewPort = new Rect(Bounds.Size);
-                var sourceSize = backingRTB.Size;
-
-                var scale = Stretch.CalculateScaling(Bounds.Size, sourceSize, StretchDirection);
-                var scaledSize = sourceSize * scale;
-                var destRect = viewPort
-                    .CenterRect(new Rect(scaledSize))
-                    .Intersect(viewPort);
-                
-                var sourceRect = new Rect(sourceSize)
-                    .CenterRect(new Rect(destRect.Size / scale));
-
-                var interpolationMode = RenderOptions.GetBitmapInterpolationMode(this);
-
-                context.DrawImage(backingRTB, sourceRect, destRect, interpolationMode);
-            }
-            
-            Dispatcher.UIThread.Post(InvalidateVisual, DispatcherPriority.Background);
-        }
-
-        /// <summary>
-        /// Measures the control.
-        /// </summary>
-        /// <param name="availableSize">The available size.</param>
-        /// <returns>The desired size of the control.</returns>
-        protected override Size MeasureOverride(Size availableSize)
-        {
-            var source = backingRTB;
-            var result = new Size();
-
-            if (source != null)
-            {
-                result = Stretch.CalculateSize(availableSize, source.Size, StretchDirection);
-            }
-
-            return result;
-        }
-
-        /// <inheritdoc/>
-        protected override Size ArrangeOverride(Size finalSize)
-        {
-            var source = backingRTB;
-
-            if (source != null)
-            {
-                var sourceSize = source.Size;
-                var result = Stretch.CalculateSize(finalSize, sourceSize);
-                return result;
-            }
-            else
-            {
-                return new Size();
-            }
-        }
-        
-        private static void SourceChanged(AvaloniaPropertyChangedEventArgs e)
-        {
-            var image = e.Sender as GifImage;
-            if (image == null)
-                return;
-
-            image.gifInstance?.Dispose();
-            image.backingRTB?.Dispose();
-            image.backingRTB = null;
-            
-            var value = e.NewValue;
-            if (value is string s)
-                value = new Uri(s);
-
-            image.gifInstance = new GifInstance();
-            image.gifInstance.SetSource(value);
-
-            image.backingRTB = new RenderTargetBitmap(image.gifInstance.GifPixelSize, new Vector(96, 96));
-        }
-    }
+		public void Dispose()
+		{
+			_isDisposed = true;
+			_bgWorker?.SendCommand(BgWorkerCommand.Dispose);
+			_targetBitmap?.Dispose();
+		}
+	}
 }
---
+++ Eto.GifImageView/GifImageView.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Eto.GifImageView
+{
+	using Eto.Forms;
+	public class GifImageView : ImageView
+	{
+		GifImage _image;
+		public GifImageView()
+		{
+		}
+		public new GifImage Image
+		{
+			get => _image;
+
+			set
+			{
+				if (_image != null)
+					_image.Dispose();
+
+				_image = value;
+
+				if (_image != null)
+					_image.OnFrameChanged += (o, e) => base.Image = _image.CurrentBitmap;
+			}
+		}
+	}
+}
